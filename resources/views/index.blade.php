e50a4face5f990dd6b9f4198e5344254c9afe1f7
macanfa108 <810193096@qq.com>
2016-12-27 19:41:45 +0800

    前端项目初始化

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>晓乎</title>
+	<link rel="stylesheet" href="public/node_modules/normalize-css/normalize.css">
+
+</head>
+<body>
+	<h1>index</h1>
+	<script src="public/node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="public/node_modules/angualr/angualr.min.js"></script>
+	<script src="public/node_modules/angualr-ui-router/release/angualr.min.js"></script>
+</body>
+</html>
