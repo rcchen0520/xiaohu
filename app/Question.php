47ce4e3ba380da399ebd06f5d2c6bedce40905b3
crc <794106825@qq.com>
2017-01-11 00:18:44 +0800

    ‘除检查用户是否存api外，用户，问题，回答，评论，通用api已经完成

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    //增加问题api
     public function add()
     {
         if(!user_ins()->is_logged_in()){
@@ -26,6 +26,7 @@ class Question extends Model
         return $this->save()?['status'=>1,'id'=>$this->id]:['status'=>0,'msg'=>'db insert failed'];
     }
 
+    //更新问题api
     public function change()
     {
         /*判断用户是否登陆*/
@@ -54,6 +55,50 @@ class Question extends Model
         }
 //        $this->user_id = session('user_id');
         return $question->save()?['status'=>1]:['status'=>0,'msg'=>'db update failed'];
+
+    }
+
+    //查看问题api
+    public function read(){
+        if(rq('id'))
+            return ['ststus' => 1,'data' => $this->find(rq('id'))];
+//        $limit = rq('limit')? : 15;
+//
+//        $skip = (rq('page')? (rq('page')-1): 0) * $limit;
+        list($limit,$skip) = paginate(rq('page'),rq('limit'));
+
+        $r = $this
+            ->orderBy('created_at')
+            ->limit($limit)
+            ->skip($skip)
+            ->get(['id' , 'title' , 'desc' , 'user_id' , 'created_at' , 'updated_at'])
+            ->keyby('id')
+        ;
+
+        return ['status'=>1,'data'=>$r];
+    }
+
+    //删除问题
+    public function remove(){
+        //判断用户是否登陆
+        if(!user_ins()->is_logged_in()){
+            return ['status' => 0,'msg' => 'login required'];
+        }
+        //判断id是否存在
+        if(!rq('id')){
+            return ['status' => 0,'msg' => 'id  is required'];
+        }
+
+        $question = $this->find(rq('id'));
+        if(!$question){
+            return ['status' => 0,'msg' => 'question not exist'];
+        }
+
+        if(session('user_id') != $question->user_id){
+            return ['status' => 0,'msg' => 'premission denied'];
+        }
+
+        return $question->delete()?['status' => 1]:['status' => 0,'msg' => 'db delete failed'];
 
     }
 }
--- app/User.php
+++ app/User.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Facades\Hash;
 
 class User extends Model
 {
-    //
+    //注册api
     public function signup()
     {
 
         /* */
         $has_username_and_password = $this->has_username_and_password();
         if(!$has_username_and_password){
-            return ['status'=>0,'msg'=>'用户名和密码皆不能为空'];
+            return err('用户名和密码皆不能为空');
         }else{
             $username = $has_username_and_password[0];
             $password = $has_username_and_password[1];
@@ -24,7 +24,7 @@ class User extends Model
         $user_exists = $this->where('username',$username)->exists();
 
         if($user_exists){
-            return ['status'=>0,'msg'=>'用户名已存在'];
+            return err('用户名已存在');
         }
 
         /**/
@@ -35,19 +35,36 @@ class User extends Model
 //        dd($user->password);
         $user->username = $username;
         if($user->save()){
-            return ['status'=>1,'id'=>$user->id];
+            return suc(['id'=>$user->id]);
         }else{
-            return ['status'=>0,'msg' =>'db insert failed'];
+            return err('db insert failed');
         }
     }
+
+    //获取用户信息api
+    public function read()
+    {
+        if(!rq('id')){
+            return err('required id');
+        }
+        $get = ['id','username','avatar_url','intro'];
+        $user = $this->find(rq('id'),$get);
+        $data = $user->toArray();
+        $answer_count = answer_ins()->where('user_id',rq('id'))->count();
+        $question_count = question_ins()->where('user_id',rq('id'))->count();
+        $data['answer_count'] = $answer_count;
+        $data['question_count'] = $question_count;
+
+        return suc($data);
+    }
     
-    /*用户登录*/
+    /*用户登录api*/
     public function login()
     {
         /*获取用户名和密码*/
         $has_username_and_password = $this->has_username_and_password();
         if(!$has_username_and_password){
-            return ['status'=>0,'msg'=>'用户名和密码皆不能为空'];
+            return err('用户名和密码皆不能为空');
         }else{
             $username = $has_username_and_password[0];
             $password = $has_username_and_password[1];
@@ -55,22 +72,23 @@ class User extends Model
         /*判断用户名是否存在*/
         $user = $this->where('username',$username)->first();
         if(!$user){
-            return ['status'=>0,'msg'=>'用户名不存在'];
+            return err('用户名不存在');
         }
 
             /*判断密码是否正确*/
             $hashed_password = $user->password;
         if(!Hash::check($password,$hashed_password)){
-            return ['status'=>0,'msg'=>'密码错误'];
+            return err('密码错误');
         }
 
         /*将用户信息写入session*/
         session()->put('username',$user->username);
         session()->put('user_id',$user->id);
 //        dd(session()->all());
-        return ['status'=>1,'id'=>$user->id];
+        return suc(['id'=>$user->id]);
     }
 
+    //判断是否有username和password
     public function has_username_and_password()
     {
         $username = rq('username');
@@ -98,6 +116,121 @@ class User extends Model
     {
         session()->forget('username');
         session()->forget('user_id');
-        return ['status'=>1];
+        return suc();
+    }
+    
+    /*修改密码*/
+    public function change_password()
+    {
+        if(!$this->is_logged_in()){
+            return err('login required');
+        }
+        /*判断是否传进旧密码和新密码*/
+        if(!rq('old_password') || !rq('new_password')){
+            return err('old_password and new_password are required');
+        }
+        /*查询当前用户*/
+        $user = $this->find(session('user_id'));
+        /*判断旧密码是否相符*/
+        if(!Hash::check(rq('old_password'),$user->password)){
+            return err('invalid old_password');
+        }
+        /*将新密码写入*/
+        $user->password = bcrypt(rq('new_password'));
+
+        return $user->save()?suc():err('db update failed');
+    }
+    //找回密码，生产短信验证码
+    public function reset_password()
+    {
+        if($this->is_robot()){
+            return err('max frequency reached');
+        }
+
+        if(!rq('phone')){
+            return err('phone is required');
+        }
+
+        $user = $this->where('phone',rq('phone'))->first();
+
+        if(!$user){
+            return err('invalid phone number');
+        }
+        /*生成验证码*/
+        $captcha = $this->generate_captcha();
+
+        $user->phone_captcha = $captcha;
+
+        $this->update_robot_time();//跟新生成验证码时间
+
+        if($user->save()){
+            /*如果验证码保存成功，发送验证码短信息*/
+            $this->send_sms();
+            return suc();
+        }
+        return err('db update failed');
+
+    }
+    //生成随机4位数验证码
+    public function generate_captcha()
+    {
+        return rand(1000,9999);
+    }
+    //模拟发送短信
+    public function send_sms()
+    {
+        return true;
+    }
+    /*判断前后刷新时间*/
+    public function is_robot($time=10)
+    {
+        if(!session('last_active_time')){
+            return false;
+        }
+
+        $current_time = time();
+        $last_active_time = session('last_active_time');
+
+        $elapsed = $current_time - $last_active_time;
+
+        return !($elapsed > $time);
+    }
+    //
+    public function update_robot_time()
+    {
+        session()->set('last_active_time',time());
+    }
+    //根据电话，短信验证码重置密码api
+    public function validate_reset_password()
+    {
+        //判断是否是验证机器人，防止强力破解
+        if($this->is_robot(2)){
+            return err('max frequency reached');
+        }
+
+        if(!rq('phone')||!rq('phone_captcha')||!rq('new_password')){
+            return err('phone,phone_captcha and new_password are required');
+        }
+        $user = $this->where([
+            'phone' =>  rq('phone'),
+            'phone_captcha' =>  rq('phone_captcha'),
+        ])->first();
+
+        if(!$user){
+            return err('invalid phone or invalid phone_captcha');
+        }
+        //加密新密码
+        $user->password = bcrypt(rq('new_password'));
+        //跟新重置密码时间
+        $this->update_robot_time();
+        return $user->save() ? suc():err('db update failed');
+    }
+
+    public function answers()
+    {
+        return $this
+            ->belongsToMany('App\Answer')
+            ->withPivot('vote')
+            ->withTimestamps();
     }
 }
